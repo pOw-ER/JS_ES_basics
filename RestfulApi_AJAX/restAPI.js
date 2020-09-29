d5a0a107b75d0f29c8fc0cc46f9a2fbd343b6e7b
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-09-29 23:05:32 +0200

    restApi posting data

--- RestfulApi_AJAX/restAPI.js
+++ RestfulApi_AJAX/restAPI.js
@@ -1,5 +1,6 @@
 document.querySelector('#getOne').addEventListener('click',getOne);
 document.querySelector('#getAll').addEventListener('click',getAll);
+document.querySelector('#postData').addEventListener('click',postData);
 
 function getOne (){
   var id = document.querySelector('#postId').value; // wenn wir mit id ein post zeigen möchten
@@ -62,6 +63,25 @@ function getAll (){
   xhr.send();
 }
 
+function postData(){
+  const data = {
+    userId:1,
+    title:"new title",
+    body: "new body"
+  }
+  var json = JSON.stringify(data); // wandeln wir das Objekt zu JSON format um
+  var url = "https://jsonplaceholder.typicode.com/posts";
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST',url,true);
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  xhr.onload = function(){
+    if (xhr.status === 201 && xhr.readyState ===4){ // 201 html response code = created
+      var post = xhr.responseText;
+      console.log(post);
+    }
+  }
+  xhr.send(json);
+}
 
 
 
